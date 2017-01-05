58e58065e02104dee8448637be41c788c0a56602
gumoon <huliang9033@qq.com>
2017-01-05 12:30:10 +0800

    update

--- database/migrations/2017_01_04_172641_update_users_table.php
+++ database/migrations/2017_01_04_172641_update_users_table.php
@@ -14,7 +14,7 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('tel', 50)->unique()->after('email');
+            $table->string('tel', 50)->after('email');
             $table->string('profile_image_url', 255)->nullable()->after('password');
             $table->unsignedTinyInteger('gender')->default(0)->comment('性别：0=未知，1=男，2=女')->after('profile_image_url');
             
@@ -32,7 +32,6 @@ class UpdateUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['tel', 'profile_image_url', 'gender', 'deleted_at']);
-            $table->dropUnique('users_tel_unique');
         });
     }
 }

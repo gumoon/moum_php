6e99c67a2ca8fa5d116ab233575e134f6d7431a1
huliang <huliang@jindankeji.com>
2017-04-23 18:30:32 +0800

    样式

--- resources/views/home/news/detail.blade.php
+++ resources/views/home/news/detail.blade.php
@@ -11,7 +11,8 @@
 
     <div class="container">
         <h3>{{$news->title}}</h3>
-        <span>{{substr($news->public_at, 0, 10)}}&span;&span;&span;&span;{{\moum\Models\News::$sources[$news->source]['name']}}</span>
+        <span>{{substr($news->public_at, 0, 10)}}     {{\moum\Models\News::$sources[$news->source]['name']}}</span>
+        <br/>
         {!! $news->content !!}
     </div>
 
